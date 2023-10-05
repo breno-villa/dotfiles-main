b0752b17f4891e07fd315fa5e84ff59611a733b0
breno-villa <32813196+breno-villa@users.noreply.github.com>
2023-10-04 22:06:46 -0300

    username change

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
   let 
-    username = "demeter";
+    # username = "demeter";
+    username = "breno";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
